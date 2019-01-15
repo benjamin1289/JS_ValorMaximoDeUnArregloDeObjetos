6129fc88b76be0c75fef261cfa2c96f5d10d9770
benjamin1289 <benjaminob.891206@gmail.com>
2019-01-14 22:47:05 -0600

    Script para calcular el valor maximo de un arreglo de objetos

---
+++ MaxValueArray.js
@@ -0,0 +1,23 @@
+Array.prototype.maxValue = function(key = null){
+    if(this.length == 0)
+        return 0;
+
+    if(!!!key || isNaN( parseFloat( this[0][key] ) ) )
+        return NaN;
+
+    let max = Math.max.apply(Math, this.map(function(obj) { return obj[key]; }));
+    return max;
+}
+
+
+let array = [
+				{id:1, nombre:'Roman', 		edad:12},
+				{id:2, nombre:'Rosalia', 	edad:13},
+				{id:3, nombre:'Shadea', 	edad:18},
+				{id:4, nombre:'Romario', 	edad:5 },
+				{id:4, nombre:'Alejandro', 	edad:21}
+			];
+
+let maximo = array.maxValue('edad');
+
+console.log("Valor Maximo: t", maximo)
